47c88e31357e53fb1c9bf0316c82e49509f3e88b
Ashik Chapagain <47524279+projectashik@users.noreply.github.com>
2022-12-26 13:20:58 +0545

    changed blog url

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import { useState } from 'react';
-import { loadStripe } from '@stripe/stripe-js';
-import axios from 'axios';
-import { useRouter } from 'next/router';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const router = useRouter();
@@ -13,10 +13,10 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
 
   const [item, setItem] = useState({
-    name: 'Apple AirPods',
-    description: 'Latest Apple AirPods.',
+    name: "Apple AirPods",
+    description: "Latest Apple AirPods.",
     image:
-      'https://images.unsplash.com/photo-1572569511254-d8f925fe2cbb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80',
+      "https://images.unsplash.com/photo-1572569511254-d8f925fe2cbb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80",
     quantity: 0,
     price: 999,
   });
@@ -43,7 +43,7 @@ export default function Home() {
   const createCheckOutSession = async () => {
     setLoading(true);
     const stripe = await stripePromise;
-    const checkoutSession = await axios.post('/api/create-stripe-session', {
+    const checkoutSession = await axios.post("/api/create-stripe-session", {
       item: item,
     });
     const result = await stripe.redirectToCheckout({
@@ -59,44 +59,44 @@ export default function Home() {
       <Head>
         <title>Stripe Checkout with Next.js</title>
         <meta
-          name='description'
-          content='Complete Step By Step Tutorial for integrating Stripe Checkout with Next.js'
+          name="description"
+          content="Complete Step By Step Tutorial for integrating Stripe Checkout with Next.js"
         />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        {status && status === 'success' && (
-          <div className='bg-green-100 text-green-700 p-2 rounded border mb-2 border-green-700'>
+        {status && status === "success" && (
+          <div className="bg-green-100 text-green-700 p-2 rounded border mb-2 border-green-700">
             Payment Successful
           </div>
         )}
-        {status && status === 'cancel' && (
-          <div className='bg-red-100 text-red-700 p-2 rounded border mb-2 border-red-700'>
+        {status && status === "cancel" && (
+          <div className="bg-red-100 text-red-700 p-2 rounded border mb-2 border-red-700">
             Payment Unsuccessful
           </div>
         )}
-        <div className='shadow-lg border rounded p-2 '>
+        <div className="shadow-lg border rounded p-2 ">
           <Image src={item.image} width={300} height={150} alt={item.name} />
-          <h2 className='text-2xl'>$ {item.price}</h2>
-          <h3 className='text-xl'>{item.name}</h3>
-          <p className='text-gray-500'>{item.description}</p>
-          <p className='text-sm text-gray-600 mt-1'>Quantity:</p>
-          <div className='border rounded'>
+          <h2 className="text-2xl">$ {item.price}</h2>
+          <h3 className="text-xl">{item.name}</h3>
+          <p className="text-gray-500">{item.description}</p>
+          <p className="text-sm text-gray-600 mt-1">Quantity:</p>
+          <div className="border rounded">
             <button
               onClick={onQuantityMinus}
-              className='bg-blue-500 py-2 px-4 text-white rounded hover:bg-blue-600'
+              className="bg-blue-500 py-2 px-4 text-white rounded hover:bg-blue-600"
             >
               -
             </button>
             <input
-              type='number'
-              className='p-2'
+              type="number"
+              className="p-2"
               onChange={onInputChange}
               value={item.quantity}
             />
             <button
               onClick={onQuantityPlus}
-              className='bg-blue-500 py-2 px-4 text-white rounded hover:bg-blue-600'
+              className="bg-blue-500 py-2 px-4 text-white rounded hover:bg-blue-600"
             >
               +
             </button>
@@ -105,18 +105,18 @@ export default function Home() {
           <button
             disabled={item.quantity === 0 || loading}
             onClick={createCheckOutSession}
-            className='bg-blue-500 hover:bg-blue-600 text-white block w-full py-2 rounded mt-2 disabled:cursor-not-allowed disabled:bg-blue-100'
+            className="bg-blue-500 hover:bg-blue-600 text-white block w-full py-2 rounded mt-2 disabled:cursor-not-allowed disabled:bg-blue-100"
           >
-            {loading ? 'Processing...' : 'Buy'}
+            {loading ? "Processing..." : "Buy"}
           </button>
         </div>
         <a
-          className='block text-blue-500 mt-4'
-          href='https://blog.cb-ashik.me/stripe-checkout-with-nextjs'
+          className="block text-blue-500 mt-4"
+          href="https://cb-ashik.hashnode.dev/stripe-checkout-with-nextjs"
         >
           Read Blog
         </a>
-        <div className='bg-yellow-100 text-yellow-700 p-2 mt-2 rounded border mb-2 border-yellow-700'>
+        <div className="bg-yellow-100 text-yellow-700 p-2 mt-2 rounded border mb-2 border-yellow-700">
           Use test card for testing.
           <p>Card Number: 4242 4242 4242 4242</p>
         </div>
